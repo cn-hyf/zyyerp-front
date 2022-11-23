d028bca2e561f4320dd7eb2bbf2c6535ffa6362d
huangyifeng <1254859490@qq.com>
2022-11-23 16:47:27 +0800

    挂载路由导航守卫，控制页面的访问权限，登录成功后会保存token，没有token时不能访问其他页面

--- src/router.js
+++ src/router.js
@@ -5,7 +5,7 @@ import Home from './components/Home.vue';
 
 Vue.use(Router)
 
-export default new Router({
+const router =  new Router({
   routes: [
     {
       path: '/', redirect: '/login'   // 当用户访问当前项目时，重定向到/login路径
@@ -18,3 +18,15 @@ export default new Router({
     }
   ]
 })
+
+// 挂载路由导航守卫
+router.beforeEach((to, from, next) => {
+  // to将要访问的路径；from代表从那个路径跳转而来；next是一个函数，表示放行。 next()放行，next('/login')强制跳转到/login路径
+  if (to.path === '/login') return next()
+  //获取token
+  const tokenStr = window.sessionStorage.getItem('token')
+  if (!tokenStr) return next('/login')  // token为空时表示没有登录，强制跳转到/login路径
+  next()
+})
+
+export default router
